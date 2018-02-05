f34a17b46c73fb2ce3a9b336c21168b8b4cf1206
jalal <jalal@nulab.co.jp>
2018-02-05 16:48:42 +0900

    add comments for streaming

--- backlog4s-core/src/main/scala/backlog4s/streaming/Streaming.scala
+++ backlog4s-core/src/main/scala/backlog4s/streaming/Streaming.scala
@@ -16,6 +16,18 @@ object Streaming {
   type ApiResponse[A] = ApiPrg[Response[Seq[A]]]
   type ApiStream[A] = Stream[ApiPrg, Seq[A]]
 
+  /**
+    * Build a stream that fetch some data from start to limit by each step
+    * This function covers only streaming over paginate api
+    * This is the only use case we have for backlog4s
+    * If we need to abstract more we will do it
+    * @param limit
+    * @param start
+    * @param step
+    * @param f a function that return the api call to get data from index to index + step
+    * @tparam A type of the data to get
+    * @return
+    */
   def stream[A](limit: Int, start: Int = 0, step: Int = 100)(f: (Int) => ApiResponse[A]): ApiStream[A] = {
     Stream.unfoldEval[ApiPrg, Int, Seq[A]](start) { index =>
       if (index >= limit)
