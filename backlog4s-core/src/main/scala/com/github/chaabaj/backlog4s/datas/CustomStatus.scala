9392e5c5ae97599a37ed368229da5a84c21fd408
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2019-12-09 19:10:52 +0900

    clean: readable code

--- backlog4s-core/src/main/scala/com/github/chaabaj/backlog4s/datas/CustomStatus.scala
+++ backlog4s-core/src/main/scala/com/github/chaabaj/backlog4s/datas/CustomStatus.scala
@@ -43,42 +43,22 @@ object DefaultStatusColor {
     values.find(_.hex == colorStr)
 }
 
-sealed trait CustomStatusColor extends StatusColor
+case class CustomStatusColor private (hex: String) extends StatusColor
 
 object CustomStatusColor {
-  case object Color1 extends CustomStatusColor {
-    val hex: String = "#ea2c00"
-  }
-  case object Color2 extends CustomStatusColor {
-    val hex: String = "#e87758"
-  }
-  case object Color3 extends CustomStatusColor {
-    val hex: String = "#e07b9a"
-  }
-  case object Color4 extends CustomStatusColor {
-    val hex: String = "#868cb7"
-  }
-  case object Color5 extends CustomStatusColor {
-    val hex: String = "#3b9dbd"
-  }
-  case object Color6 extends CustomStatusColor {
-    val hex: String = "#4caf93"
-  }
-  case object Color7 extends CustomStatusColor {
-    val hex: String = "#b0be3c"
-  }
-  case object Color8 extends CustomStatusColor {
-    val hex: String = "#eda62a"
-  }
-  case object Color9 extends CustomStatusColor {
-    val hex: String = "#f42858"
-  }
-  case object Color10 extends CustomStatusColor {
-    val hex: String = "#393939"
-  }
+  val color1: CustomStatusColor  = CustomStatusColor("#ea2c00")
+  val color2: CustomStatusColor  = CustomStatusColor("#e87758")
+  val color3: CustomStatusColor  = CustomStatusColor("#e07b9a")
+  val color4: CustomStatusColor  = CustomStatusColor("#868cb7")
+  val color5: CustomStatusColor  = CustomStatusColor("#3b9dbd")
+  val color6: CustomStatusColor  = CustomStatusColor("#4caf93")
+  val color7: CustomStatusColor  = CustomStatusColor("#b0be3c")
+  val color8: CustomStatusColor  = CustomStatusColor("#eda62a")
+  val color9: CustomStatusColor  = CustomStatusColor("#f42858")
+  val color10: CustomStatusColor = CustomStatusColor("#393939")
 
-  val values: Seq[CustomStatusColor] = Seq(
-    Color1, Color2, Color3, Color4, Color5, Color6, Color7, Color8, Color9, Color10
+  val values:  Seq[CustomStatusColor] = Seq(
+    color1, color2, color3, color4, color5, color6, color7, color8, color9, color10
   )
 
   def from(colorStr: String): Option[CustomStatusColor] =
